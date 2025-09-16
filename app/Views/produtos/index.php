8ab96c0c80f0444ec18782f10e0db4813bd09c53
Michel N Souza <91084191+MichelNsouza@users.noreply.github.com>
2025-09-15 23:10:05 -0300

    Update index.php Google tag (gtag.js)

--- app/Views/produtos/index.php
+++ app/Views/produtos/index.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-S231XGVM8V"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-S231XGVM8V');
+    </script>
 </head>
 <body class="bg-light">
     <div class="container py-5">
